b6de287088e93b7bddc1da10807fcf575cb468d3
SaiRah47 <rahulsaipratap@gmail.com>
2020-04-14 16:28:32 +0530

    Updates

---
+++ precache-manifest.5c0065d29e0437c4633ff189d4ff2030.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c3b0cfdcb4598c326b83c70fe583ac",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "b7f55eec6fc68a6339d1",
+    "url": "/Covid-19/static/css/main.c29f210d.chunk.css"
+  },
+  {
+    "revision": "20522ad07cd708b5d0a8",
+    "url": "/Covid-19/static/js/2.09169b28.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19/static/js/2.09169b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f55eec6fc68a6339d1",
+    "url": "/Covid-19/static/js/main.9f258157.chunk.js"
+  },
+  {
+    "revision": "fd4ff9d9496ad287d055",
+    "url": "/Covid-19/static/js/runtime-main.2de43cd2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19/static/media/image.d7265326.png"
+  }
+]);
